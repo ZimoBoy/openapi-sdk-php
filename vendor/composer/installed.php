57807bf07f280f990b5457b47aa357a83b3a90cf
zhaoxuegang <zhaoxuegang@mail.dataoke.com>
2021-07-19 17:14:33 +0800

    1

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-develop',
+    'version' => 'dev-develop',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '697a35d5ac785cf0e823d4849212305cb9eab658',
+    'name' => 'dtk/openapi-sdk-php',
+  ),
+  'versions' => 
+  array (
+    'dtk/openapi-sdk-php' => 
+    array (
+      'pretty_version' => 'dev-develop',
+      'version' => 'dev-develop',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '697a35d5ac785cf0e823d4849212305cb9eab658',
+    ),
+  ),
+);
